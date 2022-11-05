d3def63e966ce443606f9f563b1cc077e4f0dbeb
Alexis 'seith' M <amontelimart@gmail.com>
2022-11-05 15:10:10 +0100

    :sparkles: add: added mandatory or optional

--- src/api/authentification/dto/signIn.dto.ts
+++ src/api/authentification/dto/signIn.dto.ts
@@ -2,12 +2,10 @@ import { Trim } from 'class-sanitizer';
 import { IsEmail, IsString } from 'class-validator';
 
 export class SignInDto {
+	@Trim()
+	@IsEmail()
+	public readonly email!: string;
 
-    @Trim()
-    @IsEmail()
-    public readonly email: string;
-
-    @IsString()
-    public readonly password: string;
-
+	@IsString()
+	public readonly password!: string;
 }
