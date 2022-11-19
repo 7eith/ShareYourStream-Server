61af0c8dc32f0a01aebf2615a22f21e4191c38d7
Alexis 'seith' M <amontelimart@gmail.com>
2022-11-19 03:32:11 +0100

    :sparkles: add: organized types, added discord types, OAuth type for both

--- src/shared/types/type.d.ts
+++ src/shared/types/type.d.ts
@@ -3,39 +3,10 @@ type AuthentificationResponse = {
 	refresh_token: string;
 };
 
-type SpotifyAuthentificationResponse = {
+type OAuthTokenResponse = {
 	access_token: string;
 	token_type: string;
 	expires_in: number;
 	refresh_token: string;
 	scope: string;
-}
-
-type SpotifyUserProfile = {
-	id: string;
-	display_name: string;
-	email?: string;
-	product: string;
-	country: string;
-	href: string;
-	explicit_content: {
-		filter_enabled: boolean;
-		filter_locked: boolean;
-	};
-	external_urls: {
-		spotify: string;
-	};
-	followers: {
-		href: string | null;
-		total: number;
-	};
-	images: SpotifyUserProfile_Image[];
-	type: string;
-	uri: string;
-}
-
-type SpotifyUserProfile_Image = {
-	height: number | null;
-	url: string;
-	width: number | null;
 }
